194ad4427462e8c8b4df4f2d60fe84ec61388f60
Abdulrahman Ghubun <4010675@upm.edu.sa>
2025-07-08 13:49:52 +0300

    upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <main className="flex flex-col items-center justify-center min-h-[80vh] bg-gray-100">
       <h1 className="text-3xl font-bold mb-8 text-center">أهلاً بك 👋</h1>
 
       <div className="flex flex-col gap-4 max-w-sm w-2/3 md:w-1/2  space-y-4">
